1c6918759d858482e70a275209d1d03edd2cb4bc
Katythecat <kk.wilasinee@gmail.com>
2023-04-07 14:16:11 -0700

    vodka has been added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("create your project");
+        Vodka vodka=new Vodka();
+        vodka.type="vodka";
+        vodka.price="25";
+        vodka.inStock="200";
     }
 }
---
+++ src/Vodka.java
@@ -0,0 +1,3 @@
+public class Vodka {
+    String type, price, inStock;
+}
