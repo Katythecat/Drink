22e8424251c9e884b8a959df621c1afbe50ab6bd
Katythecat <kk.wilasinee@gmail.com>
2023-04-07 14:11:50 -0700

    coke has been added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("create your project");
+        Coke coke=new Coke();
+        coke.type="Cocacola";
+        coke.price="buy 4 for 15";
+        coke.event="Easter day";
     }
 }
